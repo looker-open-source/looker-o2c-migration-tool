799aea102505b8b77673e7ab67eb53a31f542e7d
yweiy10 <107270059+yweiy10@users.noreply.github.com>
2024-11-12 22:58:17 +0000

    Fix the copyright headers

--- looker_usage/lookerusage.go
+++ looker_usage/lookerusage.go
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright Google LLC All Rights Reserved.
+ * Copyright 2024 Google LLC.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://opensource.org/licenses/MIT
